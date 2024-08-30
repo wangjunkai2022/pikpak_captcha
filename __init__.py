49f3dad0480d5040342e61cc659f91f48b8a4044
xiaokai <xiaokai@kk.com>
2024-08-30 15:59:43 +0700

    crate

---
+++ __init__.py
@@ -0,0 +1,38 @@
+
+import logging
+from .captcha_2captcha import captcha_rewardVip as captcha_2captcha_rewardVip, get_token_register
+from .captcha_slide_img import captcha
+from .rapidapi.rapidapi import create_instance_and_pikpak_req, create_instance_and_pikpak_rewardVip
+
+logger = logging.getLogger("captcha")
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+logger.addHandler(handler)
+
+
+def slider_validation(url: str):
+    """
+    滑块验证
+    """
+    return captcha(url)
+
+
+def google_re_validation(url: str):
+    """
+    google 注册验证
+    """
+    try:
+        return create_instance_and_pikpak_req(url)
+    except Exception as e:
+        return get_token_register(url)
+
+
+def google_rewardVip_validation():
+    """
+    google vip操作
+    """
+    try:
+        return create_instance_and_pikpak_rewardVip()
+    except Exception as e:
+        return captcha_2captcha_rewardVip()
